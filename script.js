64c0623c036495bf0f60af25a07f540903ddf8b0
Diego Araujo <127630045+devdiegoSE@users.noreply.github.com>
2026-03-25 22:52:26 -0300

    criei botão
    Adicionei a função e lógica do botão.

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function adicionar() {
     var input = document.getElementById("inputtask");
     var task = input.value.trim();
-    if (task !== "") // Verifica se a tarefa não está vazia ou composta apenas por espaços em branco
-         {
-        var taskList = document.getElementById("tasklist"); // Obtém a referência à lista de tarefas
-        var listItem = document.createElement("li"); // Cria um novo elemento de lista (li) para a nova tarefa, ainda não exibida na tela.
-        listItem.textContent = task;    // Define o texto do item de lista como a tarefa digitada pelo usuário
-        taskList.appendChild(listItem); // Adiciona o novo item de lista à lista de tarefas, fazendo com que ele apareça na tela.
-        input.value = "";
-    } else {
-        alert("Por favor, digite uma tarefa.");
+
+    if (task !== "") {
+        var list = document.getElementById("tasklist");
+        var listitem = document.createElement("li");
+        listitem.textContent = task;
+        list.appendChild(listitem);
+        input.value ="";
+        
     }
 }
