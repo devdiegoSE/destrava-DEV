6a744272ab55ddcdbcb940cfe8a9356bd5a72922
Diego Araujo <127630045+devdiegoSE@users.noreply.github.com>
2026-03-28 23:39:09 -0300

    Persistencia dos dados
    Uso do localStorage

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function criarTarefaNaTela(task) {
+    var list = document.getElementById("tasklist");
+
+    var listitem = document.createElement("li");
+    listitem.textContent = task;
+
+    var button = document.createElement("button");
+    button.textContent = "Excluir";
+
+    button.onclick = function() {
+        list.removeChild(listitem);
+
+        tasks = tasks.filter(function(t) {
+            return t !== task;
+        });
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+
+    listitem.appendChild(button);
+    list.appendChild(listitem);
+}
+
 function adicionar() {
     var input = document.getElementById("inputtask");
     var task = input.value.trim();
 
     if (task !== "") {
-        var list = document.getElementById("tasklist");
-        var listitem = document.createElement("li");
-        listitem.textContent = task;
-        var button = document.createElement("button");
-        button.textContent = "Excluir";
-        button.onclick = function() {
-            list.removeChild(listitem);
-        };
-        listitem.appendChild(button);
-        list.appendChild(listitem);
+        tasks.push(task);
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        criarTarefaNaTela(task);
+
         input.value = "";
+        input.focus();
     }
 }
+tasks.forEach(function(task) {
+    criarTarefaNaTela(task);
+});
